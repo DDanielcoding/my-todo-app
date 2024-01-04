41ab3c9eee875da1ded67f4cc6be93381f4baefa
Daniel <draskoczi.daniel@gmail.com>
2024-01-04 17:41:25 +0100

    Implement the ADD button

--- functions.py
+++ functions.py
@@ -1,11 +1,19 @@
-import functions
-import PySimpleGUI as sg
-
-label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter a todo")
-add_button = sg.Button("Add")
+FILEPATH = "todos.txt"
 
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box,add_button]])
-window.read()
-window.close()
+def get_todos(filepath = FILEPATH):
+    """ Read a text file and return the list of
+    to-do items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write a to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
